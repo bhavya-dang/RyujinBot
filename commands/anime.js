d3c495c0ea3a58ea5069e00814725ffad16dee10
TritaxGamer <37131433+TritaxXCoder@users.noreply.github.com>
2020-05-26 12:10:27 +0530

    Added anime command

---
+++ commands/anime.js
@@ -0,0 +1,42 @@
+const Discord = require("discord.js");
+const malScraper = require("mal-scraper");
+const moment = require("moment");
+
+module.exports.run = async (bot, message, args) => {
+    let query = args.slice(22);
+    if(!query) return message.channel.send(embed = {
+        "title": "**ERROR - Missing Parameters!**",
+        "fields": [
+            {
+                "name": "Usage",
+                "value": "`r@anime <search query>`",
+                "inline": true
+            }
+        ]
+    })
+    
+    const data = malScraper.getInfoFromName(query);
+
+    let embed = new Discord.RichEmbed()
+    .setTimestamp(moment.utc().format())
+    .setFooter("Information supplied by MyAnimeList")
+    .setTitle(data.title)
+    .setURL(data.url)
+    .setDescription(`**Synopsis:**\n${data.synopsis}`)
+    .setColor("#ff1453")
+    .setThumbnail(data.picture)
+    .addField("English Title", data.englishTitle, true)
+    .addField("Genres", data.genres.join(", "))
+    .addField("Episodes", data.episodes, true)
+    .addField("Aired", data.aired, true)
+    .addField("Status". data.status, true)
+    .addField("Score", data.score)
+    .addField("Rank", data.ranked)
+    .addField("Studio(s)", data.studios.join(", "))
+
+    
+}
+
+module.exports.help = {
+name: "anime"
+}
