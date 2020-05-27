45cf401993f5d6b64163dfd1e1899d5817c10bee
TritaxGamer <37131433+TritaxXCoder@users.noreply.github.com>
2020-05-27 12:57:02 +0530

    fixed quiz.js

--- commands/quiz.js
+++ commands/quiz.js
@@ -109,7 +109,7 @@ module.exports.run = async (bot, message, args) => {
         console.log(err)
         if (
           await message.channel.awaitMessages(
-            (answer) => !item.incorrect_answers.includes(answer.content.toLowerCase()),
+            (answer) => !item.correct_answer.includes(answer.content.toLowerCase()),
             options
           )
         )
