94ad3f8ff55339a0ed847cca631f44f315fa5dd8
TritaxGamer <37131433+TritaxXCoder@users.noreply.github.com>
2020-05-28 13:46:05 +0530

    fixed unsplash embed

--- commands/unsplash.js
+++ commands/unsplash.js
@@ -1,4 +1,5 @@
 const Discord = require("discord.js");
+const moment = require("moment");
 const fetch = require("node-fetch");
 global.fetch = fetch;
 const Unsplash = require("unsplash-js").default;
@@ -15,14 +16,17 @@ module.exports.run = async (bot, message, args) => {
     .getRandomPhoto({ query: query })
     .then((res) => res.json())
     .then((data) => {
+        console.log(data)
       const embed = new Discord.RichEmbed()
         .setTitle("**Unsplash Image Search**")
-        .setURL(`${data.links.download.substring(0, data.links.download - 8)}`)
+        .setURL(`${data.links.download.slice(0, -8)}`)
+        .setColor("#000")
         .setImage(data.urls.full)
-        .addField("**Description**", data.description)
+        .addField("**Description**", data.description === null ? "None" : data.description)
         .addField("**Username**", data.user.username, true)
-        .addField("**Instagram Username**", data.user.instagram_username, true)
+        .addField("**Instagram Username**", data.user.instagram_username === null ? "None" : data.user.instagram_username, true)
         .addField("**Likes**", data.likes, true)
+        .addField("**Views**", data.views, true)
         .addField(
           "**Downloads**",
           data.downloads.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ","),
@@ -32,7 +36,9 @@ module.exports.run = async (bot, message, args) => {
           "**Download Image**",
           `[\`Here!\`](${data.links.download})`,
           true
-        );
+        )
+        .setFooter("Unsplash API")
+        .setTimestamp(moment.utc().format())
         message.channel.send(embed)
     });
 };
