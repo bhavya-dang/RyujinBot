38a479c18054d93f7ba6be055c072c6e4c7e7579
TritaxGamer <37131433+TritaxXCoder@users.noreply.github.com>
2020-05-29 12:10:52 +0530

    fixed a command

--- commands/flip.js
+++ commands/flip.js
@@ -1,25 +1,22 @@
-	const Discord = require("discord.js");
+const Discord = require("discord.js");
 
-	module.exports.run = async (bot, message, args) => {
-
-	var resultflip = Math.floor((Math.random() * 2) + 1);
-	if (resultflip == 1) {
-
-	const embedheads = new Discord.RichEmbed()
-	.setTitle("Coinflip")
-	.setColor("#F8A61C")
-	.setDescription(`The coin landed on heads!`);
-	message.channel.send(embedheads);
-	} else if (resultflip == 2) {
-
-	const embedtails = new Discord.RichEmbed()
-	.setTitle("Coinflip")
-	.setColor("#F8A61C")
-	.setDescription(`The coin landed on tails!`);
-	message.channel.send(embedtails);
-	}
-	}
-
-	module.exports.help = {
-	name: "flip"
+module.exports.run = async (bot, message, args) => {
+  let resultflip = Math.floor(Math.random() * 2 + 1);
+  if (resultflip == 1) {
+    const embedheads = new Discord.RichEmbed()
+      .setTitle("Coinflip")
+      .setColor("#F8A61C")
+      .setDescription(`The coin landed on heads!`);
+    message.channel.send(embedheads);
+  } else if (resultflip == 2) {
+    const embedtails = new Discord.RichEmbed()
+      .setTitle("Coinflip")
+      .setColor("#F8A61C")
+      .setDescription(`The coin landed on tails!`);
+    message.channel.send(embedtails);
   }
+};
+
+module.exports.help = {
+  name: "flip",
+};
