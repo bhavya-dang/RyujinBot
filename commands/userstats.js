163fc1439620dfef6ca1ea7b305d92869dbdb1b2
TritaxGamer <37131433+TritaxXCoder@users.noreply.github.com>
2020-06-14 19:07:34 +0530

    fixed userstats

--- commands/userstats.js
+++ commands/userstats.js
@@ -5,21 +5,21 @@ const ms = require('ms');
 module.exports.run = async (bot, message, args) => {
 
   let role = message.member.roles.map(r => `${r.name}`)
-  
+  let user = message.guild.member(message.mentions.users.first() || message.author);
+
   let uEmbed = new Discord.RichEmbed()
   .setTitle("User Information")
   .setColor("#e0d318")
-  .setThumbnail(message.author.displayAvatarURL)
-  .addField("**Userame**", message.member.displayName, true)
-  .addField("**Discriminator**", message.author.discriminator, true)
-  .addField("**ID**", message.author.id, true)
-  .addField("**Bot**", `${message.author.bot ? "Yes" : "No"}`, true)
-  .addField("**Role**", `${role}`, true)
-  .addField("**Status**",`${message.member.presence.status}`)
-  .addField("**Roles**", `${message.member.roles.filter(r => r.id !== message.guild.id).map(roles => `\`${roles.name}\``).join(" **|** ") || "No Roles"}`, true)
-  .addField("**Playing**", `${message.member.presence.game ? `${message.member.presence.game.state}` : "Not playing anything."}`)
-  .addField("**Acc. Created At**", `${moment.utc(message.author.createdAt).format("dddd, MMMM Do YYYY, HH:mm:ss")} (${ms(Date.now()- message.author.createdAt, {long: true})})`)
-  .addField("**Joined At**", `${moment.utc(message.member.joinedAt).format("dddd, MMMM Do YYYY, HH:mm:ss")} (${ms(Date.now()- message.member.joinedAt, {long: true})})`);
+  .setThumbnail(user.user.displayAvatarURL)
+  .addField("**Userame**", `\`${user.displayName}\``, true)
+  .addField("**Discriminator**", `\`${user.user.discriminator}\``, true)
+  .addField("**ID**", `\`${user.id}\``)
+  .addField("**Bot**", `${user.bot ? "Yes" : "No"}`, true)
+  .addField("**Status**",`\`${user.presence.status}\``)
+  .addField("**Roles**", `${user.roles.filter(r => r.id !== message.guild.id).map(roles => `\`${roles.name}\``).join(" **|** ") || "No Roles"}`, true)
+  .addField("**Playing**", `\`${user.presence.game ? `${user.presence.game.state}` : "Not playing anything."}\``)
+  .addField("**Acc. Created At**", `\`${moment.utc(user.createdAt).format("dddd, MMMM Do YYYY, HH:mm:ss")} (${ms(Date.now()- message.author.createdAt, {long: true})})\``)
+  .addField("**Joined At**", `\`${moment.utc(user.joinedAt).format("dddd, MMMM Do YYYY, HH:mm:ss")} (${ms(Date.now()- message.member.joinedAt, {long: true})})\``);
 
   message.channel.send(uEmbed);
 }
